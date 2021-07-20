9f40c8bc892d95f17d2b8c4634d6bae3a821e50d
Harrison <hprice518@gmail.com>
2021-07-20 15:42:47 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -28,4 +28,13 @@ class Song
     song.save
     song
   end
+
+def self.find_by_name(name)
+  @@all.detect { |song| song.name == name}
+end
+
+def find_or_create_by_name
+  self.create_by_name || self.find_by_name
+end
+
 end
